fbdec09388ed9f17f273be7f0fcd054806398519
jjang0308 <jang653838@naver.com>
2024-03-22 17:59:22 +0900

    second commit

--- src/main/java/WELCOME/EMRSERVICE/EmrServiceApplication.java
+++ src/main/java/WELCOME/EMRSERVICE/EmrServiceApplication.java
@@ -3,7 +3,7 @@ package WELCOME.EMRSERVICE;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+//eeeemmm
 @SpringBootApplication
 public class EmrServiceApplication {
 
