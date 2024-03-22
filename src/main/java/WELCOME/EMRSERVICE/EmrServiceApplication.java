39da38c25d941ef532e56b7133db89d3afe37d11
jjang0308 <jang653838@naver.com>
2024-03-22 17:43:17 +0900

    Initial commit

---
+++ src/main/java/WELCOME/EMRSERVICE/EmrServiceApplication.java
@@ -0,0 +1,13 @@
+package WELCOME.EMRSERVICE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmrServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmrServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/WELCOME/EMRSERVICE/EmrServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package WELCOME.EMRSERVICE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmrServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
