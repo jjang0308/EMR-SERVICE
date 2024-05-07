47abcdab4e62c9fd1ba61cffda511fabb74ca5cf
RYU <dudvndwlgus@naver.com>
2024-05-07 15:08:45 +0900

    ADD : 의사 권한 추가

--- src/main/java/WELCOME/EMRSERVICE/Service/Role.java
+++ src/main/java/WELCOME/EMRSERVICE/Service/Role.java
@@ -7,7 +7,9 @@ import lombok.Getter;
 @Getter
 public enum Role {
     ADMIN("ROLE_ADMIN"),
-    MEMBER("ROLE_MEMBER");
+    MEMBER("ROLE_MEMBER"),
+    DOCTOR("ROLE_DOCTOR");
 
     private String value;
 }
+
