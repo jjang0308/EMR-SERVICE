732148a600385bafb1ed3b2b7072aeb82114d7ee
RYU <dudvndwlgus@naver.com>
2024-05-14 21:41:31 +0900

    REFACTOR : 환자,의사 권한 부여

--- src/main/java/WELCOME/EMRSERVICE/Service/Member/MemberService.java
+++ src/main/java/WELCOME/EMRSERVICE/Service/Member/MemberService.java
@@ -1,13 +1,15 @@
 package WELCOME.EMRSERVICE.Service.Member;
 
+import WELCOME.EMRSERVICE.Domain.Doctor.Doctor;
 import WELCOME.EMRSERVICE.Domain.Member.Member;
 import WELCOME.EMRSERVICE.Dto.Member.MemberDto;
+import WELCOME.EMRSERVICE.Repository.Doctor.DoctorRepository;
 import WELCOME.EMRSERVICE.Repository.Member.MemberRepository;
+import WELCOME.EMRSERVICE.Service.Doctor.DoctorPrincipalDetails;
+import WELCOME.EMRSERVICE.Service.Role;
 import lombok.AllArgsConstructor;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -15,14 +17,14 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class MemberService implements UserDetailsService {
     private MemberRepository memberRepository;
+    private DoctorRepository doctorRepository;
 
     // 회원가입
     @Transactional
@@ -30,8 +32,7 @@ public class MemberService implements UserDetailsService {
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         memberDto.setPatient_pw(passwordEncoder.encode(memberDto.getPatient_pw()));
 
-        // 사용자가 의사일 경우 ROLE_DOCTOR 권한 부여
-        memberDto.setRole_id(Long.valueOf("2"));
+        memberDto.setRoles("ROLE_MEMBER");
 
         Member member = memberDto.toEntity();
         memberRepository.save(member);
@@ -41,15 +42,21 @@ public class MemberService implements UserDetailsService {
     }
 
     @Override
-    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<Member> memberWrapper = memberRepository.findByPatientLoginId(username);
-        Member member = memberWrapper.orElseThrow(() -> new UsernameNotFoundException("해당하는 사용자가 없습니다."));
+        Member memberEntity = memberRepository.findByPatientLoginId(username);
+        Doctor doctorEntity = doctorRepository.findByDoctorLoginId(username);
 
-        List<GrantedAuthority> authorities = new ArrayList<>();
-
-        return new User(member.getPatientLoginId(), member.getPatient_pw(), authorities);
+        if (memberEntity != null) { // 환자인 경우
+            List<GrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority(Role.MEMBER.getRole()));
+            return new MemberPrincipalDetails(memberEntity);
+        } else if (doctorEntity != null) { // 의사인 경우
+            List<GrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority(Role.DOCTOR.getRole()));
+            return new DoctorPrincipalDetails(doctorEntity);
+        } else { // 사용자가 존재하지 않는 경우
+            throw new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + username);
+        }
     }
+
 
 
     @Transactional
@@ -57,32 +64,32 @@ public class MemberService implements UserDetailsService {
         if (loginId == null) {
             throw new IllegalArgumentException("로그인 ID가 null입니다.");
         }
-        Optional<Member> optionalMember = memberRepository.findByPatientLoginId(loginId);
-        Member member = optionalMember.orElseThrow(() -> new IllegalArgumentException("회원을 찾을 수 없습니다. 로그인 ID: " + loginId));
-
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        if (!passwordEncoder.matches(currentPassword, member.getPatient_pw())) {
-            throw new IllegalArgumentException("현재 비밀번호가 일치하지 않습니다.");
-        }
-
-        String encPassword = passwordEncoder.encode(newPassword);
-        member.modify(encPassword);
+//        Optional<Member> optionalMember = memberRepository.findByPatientLoginId(loginId);
+//        Member member = optionalMember.orElseThrow(() -> new IllegalArgumentException("회원을 찾을 수 없습니다. 로그인 ID: " + loginId));
+//
+//        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+//        if (!passwordEncoder.matches(currentPassword, member.getPatient_pw())) {
+//            throw new IllegalArgumentException("현재 비밀번호가 일치하지 않습니다.");
+//        }
+//
+//        String encPassword = passwordEncoder.encode(newPassword);
+//        member.modify(encPassword);
     }
 
     @Transactional
     public void deleteMember(String password) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String loginId = ((UserDetails) authentication.getPrincipal()).getUsername();
-
-        Member member = memberRepository.findByPatientLoginId(loginId)
-                .orElseThrow(() -> new IllegalArgumentException("회원을 찾을 수 없습니다."));
-
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        if (!passwordEncoder.matches(password, member.getPatient_pw())) {
-            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
-        }
-
-        memberRepository.delete(member);
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        String loginId = ((UserDetails) authentication.getPrincipal()).getUsername();
+//
+//        Member member = memberRepository.findByPatientLoginId(loginId)
+//                .orElseThrow(() -> new IllegalArgumentException("회원을 찾을 수 없습니다."));
+//
+//        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+//        if (!passwordEncoder.matches(password, member.getPatient_pw())) {
+//            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+//        }
+//
+//        memberRepository.delete(member);
     }
 
 
