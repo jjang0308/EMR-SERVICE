44903b6fab04ea3e6bf4001045128fd4d0bc8fc2
Ryu-ji-hyeon <dudvndwlgus@naver.com>
2024-07-20 22:28:02 +0900

    Refactor : config

---
+++ src/main/java/WELCOME/EMRSERVICE/Config/JwtTokenUtil.java
@@ -0,0 +1,27 @@
+package WELCOME.EMRSERVICE.Config;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtTokenUtil {
+
+    private String secretKey = "secret123secret123secret123secret123secret123";
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10시간 유효
+                .signWith(SignatureAlgorithm.HS512, secretKey)
+                .compact();
+    }
+}
